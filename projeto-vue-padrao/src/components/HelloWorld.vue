fd479ac3454f152b84d4d165e23b93b3adab0a30
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-03-27 13:08:43 -0400

    .env sendo aprendido

--- projeto-vue-padrao/src/components/HelloWorld.vue
+++ projeto-vue-padrao/src/components/HelloWorld.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>Componente HelloWorld</h1>
+  <div>
+    <h1>Componente Olá {{ variavel }}</h1>
+    <p>URL da API: {{ url_api }}</p>
+    <p>
+      Modo: {{ modo }}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data: () => ({
+      variavel: process.env.VUE_APP_MINHA_VARIAVEL_AMBIENTE,
+      url_api: process.env.VUE_APP_API_URL,
+      modo: process.env.VUE_APP_MODO
+    })
 }
 </script>
 
