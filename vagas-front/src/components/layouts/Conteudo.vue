c20ff56888d38cd0fa78cdd0d8d25ea32a2646c3
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-04-04 16:05:19 -0400

    componentes

---
+++ vagas-front/src/components/layouts/Conteudo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Conteúdo</p>
+        <home></home>
+        <publicar-vaga></publicar-vaga>
+    </div>
+</template>
+
+<script>
+    import Home from '../views/Home.vue'
+    import PublicarVaga from '../views/PublicarVaga.vue'
+
+    export default{
+        name: 'Conteudo',
+        components: {
+            Home,
+            PublicarVaga
+        }
+    }
+</script>
+
+<style></style>
---
+++ vagas-front/src/components/views/PublicarVaga.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Publicar Vaga</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'PublicarVaga'
+    }
+</script>
+
+<style></style>
