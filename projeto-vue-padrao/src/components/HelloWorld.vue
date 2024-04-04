894e0cca605a995f52a477e90263c9753bd4538b
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-04-04 09:53:38 -0400

    asdasd

--- projeto-vue-padrao/src/components/HelloWorld.vue
+++ projeto-vue-padrao/src/components/HelloWorld.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
-    <h1>Componente Olá {{ variavel }}</h1>
-    <p>URL da API: {{ url_api }}</p>
-    <p>
-      Modo: {{ modo }}
-    </p>
-  </div>
+	<div>
+		<h1>Componente Olá {{ variavel }}</h1>
+		<p>URL da API: {{ url_api }}</p>
+		<p>
+			Modo: {{ modo }}
+		</p>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data: () => ({
-      variavel: process.env.VUE_APP_MINHA_VARIAVEL_AMBIENTE,
-      url_api: process.env.VUE_APP_API_URL,
-      modo: process.env.VUE_APP_MODO
-    })
+	name: 'HelloWorld',
+	data: () => ({
+			variavel: process.env.VUE_APP_MINHA_VARIAVEL_AMBIENTE,
+			url_api: process.env.VUE_APP_API_URL,
+			modo: process.env.VUE_APP_MODO
+		})
 }
 </script>
 
